c43b0091ccf073eb1b20c2cdc2e1707a02268b26
user <user@PAVILION-10>
2018-05-31 15:22:52 +0530

    banking application project

---
+++ BankingApplication/src/bankingapplication/Account.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankingapplication;
+
+/**
+ *
+ * @author user
+ */
+class Account {
+     int balance;
+    String name;
+    
+   void account()
+    {
+       this.balance=0;
+       this.name=null;
+    }
+}
