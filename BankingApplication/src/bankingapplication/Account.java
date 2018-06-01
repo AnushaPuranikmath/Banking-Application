7943bedf5b1d93929daf780f258d8295405829d9
user <user@PAVILION-10>
2018-06-01 08:49:25 +0530

    banking application

--- BankingApplication/src/bankingapplication/Account.java
+++ BankingApplication/src/bankingapplication/Account.java
@@ -6,12 +6,17 @@
 package bankingapplication;
 
 /**
- *
- * @author user
+ * An class account forms a template for an object of bank account.
+ * @author Anusha
  */
 class Account {
      int balance;
     String name;
+    
+    /**
+     * It is the constructor to initialize account name and balance 
+     * The name and balance are initialized to zero at the beginning  
+     */
     
    void account()
     {
